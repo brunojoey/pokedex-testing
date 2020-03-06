22b213d52c96f1527bb9b5288da35a90bb0c5f93
Joseph Bruno <brunojoey12@gmail.com>
2020-03-06 17:35:38 -0600

    Added Javascript to begin the process.

---
+++ script.js
@@ -0,0 +1,23 @@
+var pokedexContainer = document.querySelector(".container");
+var amountPokemon = 890;
+var pokemonTypes = [ "Fire", "Grass", "Water", "Electric", "Poison", "Rock", "Ground", "Steel", "Dark", "Fairy", "Ice", "Dragon", "Ghost", "Normal", "Fighting", "Bug", "Flying", "Psycic"];
+var pokemonTypeColors = [
+    Fire = "red",
+    Water = "blue",
+    Grass = "green",
+    Electric = "yellow",
+    Poison = "indigo",
+    Rock = "brown",
+    Ground = "light brown",
+    Steel = "grey",
+    Dark = "black",
+    Fairy = "pink",
+    Ice = "cyan",
+    Dragon = "navy blue",
+    Ghost = "dark purple",
+    Normal = "white",
+    Fighting = "orange",
+    Flying = "light blue",
+    Psychic = "purple"
+]
+
