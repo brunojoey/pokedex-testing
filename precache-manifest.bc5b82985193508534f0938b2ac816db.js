8c0a490c564eae82dc535c90dc7b9c1c2a1e0f19
Joey Bruno <brunojoey12@gmail.com>
2020-07-28 13:45:56 -0500

    Updates

---
+++ precache-manifest.bc5b82985193508534f0938b2ac816db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ede7371d16b0e4db114e8836c534a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37d0194fa626d94783b0",
+    "url": "/static/css/main.8e5b9307.chunk.css"
+  },
+  {
+    "revision": "6245db9c2744ab533e32",
+    "url": "/static/js/2.79106b86.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.79106b86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d0194fa626d94783b0",
+    "url": "/static/js/main.d7c9c0e8.chunk.js"
+  },
+  {
+    "revision": "df98edcf244a912541ea",
+    "url": "/static/js/runtime-main.1a63fd23.js"
+  }
+]);
