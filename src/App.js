6b488b679e14d574e147e07c0d0f42b10d683842
Joey Bruno <brunojoey12@gmail.com>
2021-07-02 11:04:34 -0500

    Added Router to App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import Pokemon from './pages/Pokemon';
 import RegionPage from './pages/Region';
 import RegionDex from './pages/RegionDex';
 // import Types from './pages/Types';
-import { Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
-const App = (props) => {
-
+const App = () => {
   return (
-    <Switch>
-      <Route exact path='/' component={Pokedex} />
-      <Route path='/regions' render={(props) => <RegionDex {...props} />} />
-      <Route path='/regions/:regionId' render={(props) => <RegionPage {...props} />} />
-      <Route path='/pokemon/:pokemonId' render={(props) => <Pokemon {...props}/>} />
-    </Switch>
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Pokedex} />
+        <Route path='/regions' render={(props) => <RegionDex {...props} />} />
+        <Route path='/regions/:regionId' render={(props) => <RegionPage {...props} />} />
+        <Route path='/pokemon/:pokemonId' render={(props) => <Pokemon {...props}/>} />
+      </Switch>
+    </Router>
   );
 };
 
